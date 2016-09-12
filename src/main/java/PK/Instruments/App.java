52a01a2f45de48a75a5a01de1f652ed9bcab9a30
hadook <hadook13@gmail.com>
2016-09-12 20:45:13 +0200

    git init

---
+++ src/main/java/PK/Instruments/App.java
@@ -0,0 +1,13 @@
+package PK.Instruments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
